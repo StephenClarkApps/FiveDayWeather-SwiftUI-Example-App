e6a3824d976544f1d4e1c31f54d8423828c7001e
Stephen Clark <stephen.clark2@city-holdings.co.uk>
2022-02-21 11:37:30 +0000

    Initial Commit.  SwiftUI Project created, added testing bundles.

---
+++ FiveDayWeather/FiveDayWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  FiveDayWeatherApp.swift
+//  FiveDayWeather
+//
+//  Created by Stephen Clark on 21/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct FiveDayWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
